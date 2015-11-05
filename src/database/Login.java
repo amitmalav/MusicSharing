d1e364577df659d0f2a8cefcd9ec13d79c39a41e
amitmalav <amitmalav96@gmail.com>
2015-11-05 23:24:26 +0530

    first

---
+++ src/database/Login.java
@@ -0,0 +1,58 @@
+package database;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String n=request.getParameter("username");  
+	    String p=request.getParameter("password");  
+	          
+	    if(LoginAct.validate(n, p)){  
+	        RequestDispatcher rd=request.getRequestDispatcher("Homepage");  
+	        rd.forward(request,response);
+	    }  
+	    else{  
+	        out.print("Sorry username or password error");  
+	        RequestDispatcher rd=request.getRequestDispatcher("login.jsp");  
+	        rd.include(request,response);  
+	    }  
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/database/LoginAct.java
@@ -0,0 +1,54 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginAct {
+	
+	public static boolean validate(String name,String pass){  
+		boolean status=false;
+		Connection connection=null;
+		
+		try{
+		connection=getConnection();
+		PreparedStatement ps = connection.prepareStatement("select * from users where username=? and password=?");  
+		ps.setString(1,name);  
+		ps.setString(2,pass);  
+		      
+		ResultSet rs=ps.executeQuery();  
+		status=rs.next();
+		}catch(Exception e){System.out.println(e);}  
+		return status;  
+	}
+	
+	
+	
+	static Connection getConnection() {
+		String dbURL = "jdbc:postgresql://10.105.1.12/cs387";
+        String dbUser = "db130050045";
+        String dbPass = "db130050045";
+        Connection connection=null;
+        try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+        } catch(ClassNotFoundException cnfe){
+        	System.out.println("JDBC Driver not found");
+        } catch(SQLException sqle){
+        	System.out.println("Error in getting connetcion from the database");
+        }
+        
+        return connection;
+	}
+	
+	static void closeConnection(Connection connection) {
+		try{
+			connection.close();
+		} catch(SQLException sqle) {
+			System.out.println("Error in close database connetcion");
+		}
+	}
+
+}
